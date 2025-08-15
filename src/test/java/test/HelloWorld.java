351a09c571cbe74655cfcf83df56e561ec5b3419
jvirginiyavictor <j.virginiya.victor@gmail.com>
2025-08-15 15:04:39 +0530

    first commit

---
+++ src/test/java/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+
+	@Test
+	public void printHello() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
